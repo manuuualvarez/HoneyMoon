b357ea4cfd0c01f52526f13b132795812ae91d75
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-06-29 15:42:35 -0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Manny Alvarez on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
